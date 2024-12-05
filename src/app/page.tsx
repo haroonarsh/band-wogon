9038cef42cb8f261eb90782d414145d525e1e854
Haroon Arshad <arshadharoon216@gmail.com>
2024-12-05 23:45:10 +0500

    create next.js app & create folder structure.

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import SignupPage from "./pages/auth/signup/SignupPage.jsx";
+
+export default function Home() {
+  return (
+   <>
+   <SignupPage /> 
+   </>
+  );
+}
