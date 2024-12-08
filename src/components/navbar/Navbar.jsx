e9e7f0cebbd87f083f120f0bf8f2dc971a26898a
Haroon Arshad <arshadharoon216@gmail.com>
2024-12-08 23:58:40 +0500

    create a navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,94 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './navbar.module.css'
 
 function Navbar() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  }
   return (
     <>
       <div className={styles.navbar}>
         <img className={styles.img} src="./icons/instant_mix.png" alt="" />
-        <div>
+        <div className={`${isOpen ? styles.genre : ''}`} 
+        onClick={toggleDropdown}>
           <img className={styles.img} src="./icons/genres.png" alt="" />
           <p>Genre</p>
           <img className={styles.arrow} src="./icons/arrow_drop_down.png" alt="" />
         </div>
+
+            {/* // dropdown  */}
+        {isOpen && (
+          <ul className={styles.dropdown}>
+          <li>
+        <input type="checkbox" id={styles.dropdown} name="dropdown" />
+          <p>Classical</p>
+          </li>
+          <li>
+          <input type="checkbox" id={styles.dropdown} name="dropdown" />
+          <p>Blues</p>
+          </li>
+          <li>
+          <input type="checkbox" id={styles.dropdown} name="dropdown" />
+          <p>EDM</p>
+          </li>
+          <li>
+          <input type="checkbox" id={styles.dropdown} name="dropdown" />
+          <p>Country</p>
+          </li>
+          <li>
+          <input type="checkbox" id={styles.dropdown} name="dropdown" />
+          <p>Funk</p>
+          </li>
+          <li>
+          <input type="checkbox" id={styles.dropdown} name="dropdown" />
+          <p>Folk</p>
+          </li>
+          <li>
+          <input type="checkbox" id={styles.dropdown} name="dropdown" />
+          <p>Jazz</p>
+          </li>
+          <li>
+          <input type="checkbox" id={styles.dropdown} name="dropdown" />
+          <p>Hip-Hop</p>
+          </li>
+          <li>
+          <input type="checkbox" id={styles.dropdown} name="dropdown" />
+          <p>Metal</p>
+          </li>
+          <li>
+          <input type="checkbox" id={styles.dropdown} name="dropdown" />
+          <p>Latin</p>
+          </li>
+          <li>
+          <input type="checkbox" id={styles.dropdown} name="dropdown" />
+          <p>Punk</p>
+          </li>
+          <li>
+          <input type="checkbox" id={styles.dropdown} name="dropdown" />
+          <p>Pop</p>
+          </li>
+          <li>
+          <input type="checkbox" id={styles.dropdown} name="dropdown" />
+          <p>Rock</p>
+          </li>
+          <li>
+          <input type="checkbox" id={styles.dropdown} name="dropdown" />
+          <p>Reggae</p>
+          </li>
+          <li>
+          <input type="checkbox" id={styles.dropdown} name="dropdown" />
+          <p>Soul</p>
+          </li>
+          <li>
+          <input type="checkbox" id={styles.dropdown} name="dropdown" />
+          <p>R&B</p>
+          </li>
+        </ul>
+        )}
+
         <div>
           <img className={styles.img} src="./icons/schedule.png" alt="schelude" />
           <p>Time</p>
