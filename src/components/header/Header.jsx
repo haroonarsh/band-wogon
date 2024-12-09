b4c65f4773b2ec7f83a3c84c7c9a48a54f0766ec
Haroon Arshad <arshadharoon216@gmail.com>
2024-12-09 23:54:04 +0500

    improved header style

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styles from './header.module.css'
 import { IoMdSearch } from "react-icons/io";
+import { LuArrowLeft } from "react-icons/lu";
+import { RxCross2 } from "react-icons/rx";
+
 
 function Header() {
+
+  const [open, setOpen] = useState(false)
+  const dropdownRef = useRef(null)
+
+  const toggleDropdown = (e) => {
+    e.preventDefault();
+    setOpen(!open);
+  }
+
+  const closeDropdown = (e) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+      setOpen(false);
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousedown', closeDropdown);
+    return () => document.removeEventListener('mousedown', closeDropdown);
+  }, []);
   return (
     <>
         <header className={styles.header}>
@@ -10,10 +32,47 @@ function Header() {
           <img src="app-icon.png" alt="" />
           <h1 className='heading_4_regular'>BandWagon</h1>
         </div>
-        <div className={styles.search}>
+        <div className={styles.search}
+        onClick={toggleDropdown}
+        >
           <IoMdSearch className={styles.icon}/>
           <input type="text" placeholder='Search for Artist, Venues. or Addresses' />
         </div>
+        {open && (
+          <div className={styles.search2} ref={dropdownRef}>
+          <div className={styles.search}>
+            <LuArrowLeft className={styles.icon}
+            onClick={toggleDropdown}/>
+            <input type="text" placeholder='Search for Artist, Venues. or Addresses' />
+            </div>
+            <p className={styles.recent}>Recent searches</p>
+  
+            <div className={styles.recent_search}>
+              <div className={styles.searches}>
+              <img src="./images/img.png" alt="" />
+              <h2>Allen Ruppersberg</h2>
+              </div>
+              <RxCross2 className={styles.icon}/>
+            </div>
+            <div className={styles.recent_search}>
+              <div>
+              <img src="./images/img.png" alt="" />
+              <h2>Allen Ruppersberg</h2>
+              </div>
+              <RxCross2 className={styles.icon}/>
+            </div>
+  
+            <div className={styles.recent_search2}>
+              <p>555 55th St N, State Zip</p>
+              <RxCross2 className={styles.icon2}/>
+            </div>
+            <div className={styles.recent_search2}>
+              <p>BandWagon, 555 55th St N, City St...</p>
+              <RxCross2 className={styles.icon2}/>
+            </div>
+          
+          </div>
+        )}
         <div className={styles.profile}>
           <img src="./images/profile.png" alt="" />
         </div>
