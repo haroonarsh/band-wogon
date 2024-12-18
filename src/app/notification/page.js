0398b9b21f53e22b63427ae17197c1733542e9d9
Haroon Arshad <arshadharoon216@gmail.com>
2024-12-18 22:29:47 +0500

    create notification page

---
+++ src/app/notification/page.js
@@ -0,0 +1,55 @@
+"use client"
+
+import Header from '@/components/header/Header'
+import Sidebar from '@/components/sidebar/Sidebar'
+import React from 'react'
+import styles from './notification.module.css'
+import { PiDotsThreeOutlineFill } from "react-icons/pi";
+
+function page() {
+  return (
+    <>
+             {/* Header */}
+        <Header />
+            {/* Sidebar */}
+        <Sidebar />
+
+            {/* Main */}
+        <div className={styles.main}>
+            <div className={styles.blank}><div></div></div>
+            <div className={styles.main_content}>
+                <h1>Notifications</h1>
+                <div className={styles.notification}>
+                    <img src="./images/Image(1).png" alt="" />
+                    <div className={styles.notification_info}>
+                        <p>Three of your favourite artist are playing live in your area!</p>
+                        <p className={`${'neutral5'} ${styles.time}`}>2 hours ago</p>
+                    </div>
+                    <PiDotsThreeOutlineFill size={27} className={styles.dots} />
+                    <span className={styles.dot}></span>
+                </div>
+                <div className={styles.notification}>
+                    <img src="./images/Image(1).png" alt="" />
+                    <div className={styles.notification_info}>
+                        <p>Three of your favourite artist are playing live in your area!</p>
+                        <p className={`${'neutral5'} ${styles.time}`}>20 hours ago</p>
+                    </div>
+                    <PiDotsThreeOutlineFill size={27} className={styles.dots} />
+                    {/* <span className={styles.dot}></span> */}
+                </div>
+                <div className={styles.notification}>
+                    <img src="./images/Image(1).png" alt="" />
+                    <div className={styles.notification_info}>
+                        <p>Three of your favourite artist are playing live in your area!</p>
+                        <p className={`${'neutral5'} ${styles.time}`}>2 day ago</p>
+                    </div>
+                    <PiDotsThreeOutlineFill size={27} className={styles.dots} />
+                    {/* <span className={styles.dot}></span> */}
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default page
