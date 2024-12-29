4b0150eedbf1d3beafe185e8909597fe2883a0f0
Haroon Arshad <arshadharoon216@gmail.com>
2024-12-30 00:51:05 +0500

    in this file create or update the user code with google

---
+++ frontend/src/backend/utils/createOrUpdatauser.js
@@ -0,0 +1,25 @@
+import User from "../models/user.model";
+
+const createOrUpdateUser = async (user, account, profile) => {
+    const profileImage = account.provider === 'google' ? profile.picture : user.profileImage;
+    let existingUser = await User.findOne({ email: user.email });
+  
+    if (!existingUser) {
+      existingUser = new User({
+        email: user.email,
+        name: profile.name || user.name,
+        username: user.name || `user_${account.providerAccountId}`,
+        profileImage: profileImage || null,
+        googleId: account.provider === 'google' ? account.providerAccountId : "",
+        location: profile?.location || "Unknown",
+      });
+      await existingUser.save();
+    } else if (account.provider === 'google' && !existingUser.profileImage) {
+      existingUser.profileImage = profileImage;
+      await existingUser.save();
+    }
+  
+    return existingUser;
+  };
+  
+export default createOrUpdateUser;
