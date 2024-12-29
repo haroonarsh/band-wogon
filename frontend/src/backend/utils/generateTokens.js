d02884d876530ced5f64c735fd848e67fcf8b3cc
Haroon Arshad <arshadharoon216@gmail.com>
2024-12-30 00:42:51 +0500

    generate token code available in this file

---
+++ frontend/src/backend/utils/generateTokens.js
@@ -0,0 +1,7 @@
+import jwt from "jsonwebtoken";
+
+export const generateTokens = (user) => {
+    const accessToken = jwt.sign({ id: user._id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1d" });
+    const refreshToken = jwt.sign({ id: user._id }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "7d" });
+    return { accessToken, refreshToken };
+} 
