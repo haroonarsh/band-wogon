b4471de1360a1e85439b0b1b227389592d3a5a89
Haroon Arshad <arshadharoon216@gmail.com>
2025-01-07 10:53:10 +0500

    create artust model

---
+++ backend/src/models/artist.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const artistSchema = new mongoose.Schema({
+    profileImage: {
+        type: String,
+        required: false,
+    },
+    artistName: {
+        type: String,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    bio: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    startDate: {
+        type: Date,
+        required: true,
+    },
+    showPerformed: {
+        type: String,
+        required: true,
+        default: 0,
+    },
+    genres: {
+        type: String,
+        enum: ['Blues', 'Classical', 'Country', 'EDM', 'Folk', 'Funk', 'Hip-Hop', 'Jazz', 'Latin', 'Metal', 'Pop', 'Punk', 'Reggae', 'R&B', 'Rock', 'Soul'],
+        validate: {
+            validator: function (array) {
+                return array.length >= 1;
+            },
+            message: 'At least one genre must be selected.'
+        },
+        required: true,
+    },
+}, { timestamps: true }
+);
+
+const Artist = mongoose.models.Artist || mongoose.model("Artist", artistSchema);
+
+export default Artist;
