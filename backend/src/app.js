9f9843af37fb166c47fd0a819991c9f18b427015
Haroon Arshad <arshadharoon216@gmail.com>
2025-01-08 23:10:10 +0500

    add some changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,30 +5,31 @@ import bodyParser from "body-parser";
 import dotenv from "dotenv";
 
 dotenv.config({
-    path: "./.env"
+    path: "./env"
 })
 
 const app = express();
 
         // Middleware
 app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 app.use(bodyParser.json());
 
+
         // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log("Connected to MongdoDB"))
+mongoose.connect(process.env.MONGODB_URL)
+.then(() => console.log("Connected to MongoDB"))
 .catch((error) => console.error("Error connecting to MongoDB:", error));
 
         // Routes
-const userRoutes = require("./routers/user.routes");
+import userRoutes from "./routes/user.routes.js"
 app.use("/api/user", userRoutes);
 
         // Default route
 app.get("/", (req, res) => {
-    res.send("Backend is running...");
+    res.send("Backend is running...");  
 })
 
         // Start the server
