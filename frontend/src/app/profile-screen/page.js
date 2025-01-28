61088319b9cef1016a6d0ef063521739a1828223
Haroon Arshad <arshadharoon216@gmail.com>
2025-01-28 23:30:44 +0500

    make is responsive

--- frontend/src/app/profile-screen/page.js
+++ frontend/src/app/profile-screen/page.js
@@ -7,6 +7,8 @@ import Sidebar from '@/components/sidebar/Sidebar'
 import { FaAngleRight } from "react-icons/fa6";
 // import { useState, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
+import { FaArrowLeft } from "react-icons/fa";
+import { IoSettingsSharp } from "react-icons/io5";
 
 function Page() {
 
@@ -24,14 +26,23 @@ function Page() {
     return (
         <>
                   {/* Header */}
+                <div className={styles.header}>
             <Header />
-
+            </div>
                     {/* sidebar */}
             <Sidebar />
 
             {/* Main */}
         <div className={styles.main}>
             <div className={styles.blank}><div></div></div>
+            <div className={styles.title}>
+                <FaArrowLeft className={styles.icon}
+                onClick={() => router.push('/home')}
+                />
+                <IoSettingsSharp className={styles.icon} 
+                onClick={() => router.push('/setting')} 
+                />
+            </div>
             <div className={styles.main_content}>
                 <div className={styles.artist_profile}>
                 <div className={styles.profile}>
@@ -44,7 +55,9 @@ function Page() {
                     onClick={() => router.push('/edit-profile')}
                     >Edit profile</button>
                 </div>
-                <div className={styles.saved}>
+                <div className={styles.saved}
+                onClick={() => router.push('/saved-and-hidden-artists')}
+                >
                     <p>View saved & hidden artists</p> 
                     <FaAngleRight />
                 </div>
