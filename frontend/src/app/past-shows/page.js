2d66f8685958cefa15c65e484cce2df77e9ffa5c
Haroon Arshad <arshadharoon216@gmail.com>
2025-01-31 00:01:13 +0500

    now this page is responsive

--- frontend/src/app/past-shows/page.js
+++ frontend/src/app/past-shows/page.js
@@ -7,18 +7,29 @@ import { MdOutlineLocationOn } from "react-icons/md";
 import { MdOutlineCalendarMonth } from "react-icons/md";
 import { FaRegClock } from "react-icons/fa";
 import { FaPlus } from "react-icons/fa";
+import { FaArrowLeft } from 'react-icons/fa';
+import { useRouter } from 'next/navigation';
 
 function Page() {
+
+    const router = useRouter();
     return (
         <>
                 {/* Header */}
-            <Header />
+            <div className={styles.header}>
+                <Header />
+            </div>
 
                 {/* Main */}
             <div className={styles.main}>
                 <div className={styles.blank}><div></div></div>
                 <div className={styles.main_content}>
+                <div className={styles.heading_div}>
+                    <FaArrowLeft className={styles.icon}
+                    onClick={() => router.push('/upcoming-schedules')}
+                    />
                     <h1 className={styles.heading}>Past shows</h1>
+                    </div>
                     <div className={styles.artist}>
                         <img src="./images/frame2.png" alt="" />
                         <h2>BandWagon music</h2>
