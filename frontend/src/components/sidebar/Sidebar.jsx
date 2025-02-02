c6dba900c47073b40dd53639deb5eb8667355e5f
Haroon Arshad <arshadharoon216@gmail.com>
2025-02-02 13:45:06 +0500

    add artist

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+'use client';
+
+import React, { useEffect, useState } from 'react'
 import styles from './sidebar.module.css'
 import { MdOutlineLocationOn } from "react-icons/md";
 import { MdOutlinePersonOutline } from "react-icons/md";
@@ -7,6 +9,7 @@ import { IoMdSettings } from "react-icons/io";
 import { TbLogout } from "react-icons/tb";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
+import { GiGuitarHead } from "react-icons/gi";
 import { usePathname, useRouter } from 'next/navigation';
 import axios from 'axios';
 
@@ -15,7 +18,24 @@ function Sidebar() {
     const [showSidebar, setShowSidebar] = useState(false);
     const router = useRouter();
     const pathname = usePathname();
+    const [user, setUser] = useState(null);
 
+    console.log("USER : ",user);
+
+            // Check if user data exists in localStorage
+  useEffect(() => {
+          // Get the user data from localStorage
+          const storedUser = localStorage.getItem('userData');
+          const googeleUser = localStorage.getItem('user');
+          console.log("storedUser", storedUser);
+            console.log("googeleUser", googeleUser);
+          
+          if (storedUser || googeleUser) {
+              setUser(JSON.parse(storedUser) || JSON.parse(googeleUser));
+          }
+      }, [])
+
+            // logout function
     const handleLogout = () => {
         const removeUser = localStorage.removeItem('user');
         localStorage.removeItem('userData');
@@ -49,7 +69,7 @@ function Sidebar() {
                 >
                     <MdOutlineLocationOn className={styles.icon}/>
                 </div>
-                <div className={`${pathname === '/profile-screen' ? styles.active : styles.profile}`}
+                <div className={`${pathname === '/profile-screen' || pathname === '/edit-profile' ? styles.active : styles.profile}`}
                 onClick={() => router.push('/profile-screen')}
                 >
                     <MdOutlinePersonOutline className={styles.icon_2}/>
@@ -64,6 +84,13 @@ function Sidebar() {
                 >
                     <IoMdSettings className={styles.icon4}/>
                 </div>
+                {user && (
+                    <div className={`${user?.role === 'artist' ? styles.show : styles.hide} ${pathname === '/artist-page-management' || pathname === '/create-page' ? styles.active : styles.artist}`}
+                    onClick={() => router.push('/artist-page-management')}
+                    >
+                        <GiGuitarHead className={styles.icon_2}/>
+                    </div>
+                )}
                 <div className={styles.move_item}
                 onClick={handleLogout || logoutGoogle}
                 >
@@ -79,22 +106,33 @@ function Sidebar() {
                     {/* // Show sidebar */}
             {showSidebar && (
                 <div className={styles.show_sidebar}>
-                <div className={styles.show_location}>
-                <MdOutlineLocationOn className={styles.icon2}/>
-                <p>Explore</p>
+                <div className={styles.show_location}
+                onClick={() => router.push('/home')}>
+                <MdOutlineLocationOn className={`${pathname === '/home' ? styles.icon2 : styles.icon}`}/>
+                <p className={`${pathname === '/home' ? styles.active3 : ''}`}>Explore</p>
                 </div>
-                <div className={styles.show_profile} >
-                <MdOutlinePersonOutline className={styles.icon}/>
-                <p>Profile</p>
+                <div className={styles.show_profile} 
+                onClick={() => router.push('/profile-screen')}>
+                <MdOutlinePersonOutline className={`${pathname === '/profile-screen' || pathname === '/edit-profile' ? styles.icon2 : styles.icon}`}/>
+                <p className={`${pathname === '/profile-screen' || pathname === '/edit-profile' ? styles.active3 : styles.inactive}`}>Profile</p>
                 </div>
-                <div className={styles.show_notification}>
-                <MdNotificationsNone className={styles.icon}/>
-                <p>Notifications</p>
+                <div className={styles.show_notification}
+                onClick={() => router.push('/notification')}>
+                <MdNotificationsNone className={`${pathname === '/notification' ? styles.icon2 : styles.icon}`}/>
+                <p className={`${pathname === '/notification' ? styles.active3 : styles.inactive}`}>Notifications</p>
                 </div>
-                <div className={styles.show_sitting}>
-                <IoMdSettings className={styles.icon}/>
-                <p>Settings</p>
+                <div className={styles.show_sitting}
+                onClick={() => router.push('/setting')}>
+                <IoMdSettings className={`${pathname === '/setting' ? styles.icon2 : styles.icon}`}/>
+                <p className={`${pathname === '/setting' ? styles.active3 : styles.inactive}`}>Settings</p>
                 </div>
+                {user.role === 'artist' && (
+                    <div className={styles.show_profile}
+                    onClick={() => router.push('/artist-page-management')}>
+                    <GiGuitarHead className={`${pathname === '/artist-page-management' || pathname === '/create-page' ? styles.icon2 : styles.icon}`}/>
+                    <p className={`${pathname === '/artist-page-management' || pathname === '/create-page' ? styles.active3 : styles.inactive}`}>Artist page</p>
+                    </div>
+                )}
                 <div className={styles.show_move_item}
                 onClick={handleLogout || logoutGoogle}
                 >
