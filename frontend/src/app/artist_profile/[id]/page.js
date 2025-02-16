fa8b08f97cdad3df73ca5b8bc7f0fc0fb2a5e8f8
Haroon Arshad <arshadharoon216@gmail.com>
2025-02-16 22:54:42 +0500

    added artist profile fetching functioanilty

--- frontend/src/app/artist_profile/[id]/page.js
+++ frontend/src/app/artist_profile/[id]/page.js
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './artist_profile.module.css'
 import { MdOutlineLocationOn } from "react-icons/md";
 import { GiMusicSpell } from "react-icons/gi";
@@ -16,14 +16,45 @@ import { RiMoneyDollarBoxFill } from "react-icons/ri";
 import { FaVimeoV } from "react-icons/fa6";
 import { FaRegCheckCircle } from "react-icons/fa";
 import { RxCross2 } from "react-icons/rx";
-import { useRouter } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
+import axios from 'axios';
 
 function Artist_profile() {
 
   const [isChecked, setIsChecked] = useState(false);
   const router = useRouter();
+  const { id } = useParams();
+  const [artist, setArtist] = useState(null);
+  const [loading, setLoading] = useState(true);
 
+          // Fetch artist data
+  useEffect(() => {
+    const fetchArtist = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8000/api/user/get-artist/:?id=${id}`);
+        console.log("response", response);
+        
+        if (response.data.data) {
+          setArtist(response.data.data);
+        }
+      } catch (error) {
+        
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchArtist();
+  }, [id]);
+
+  if (loading) {
+    return (
+      <div>Loading...</div>
+    );
+  }
+
+          // Function to handle checkbox change
   const handleCheckboxChange = () => {
     setIsChecked(!isChecked);
   }
@@ -34,24 +65,24 @@ function Artist_profile() {
         <div className={styles.artist}>
           <div className={styles.header}>
         <MdOutlineKeyboardArrowLeft className={styles.icon3} onClick={() => router.push('/home')} />
-          <h1 className='heading_3_medium'>andwarhool234</h1>
+          <h1 className='heading_3_medium'>{artist?.artistName || 'andwarhool234'}</h1>
           </div>
           <div className={styles.artist_details}>
-            <img src="./images/Image(1).png" alt="" className={styles.artist_image} />
+            <img src={artist?.artistImage || "./images/Image(1).png"} alt="" className={styles.artist_image} />
             <div className={styles.artist_info}>
               <div className={styles.artist_name}>
-                <h2 className='heading_6_medium'>andwarhool234</h2>
+                <h2 className='heading_6_medium'>{artist?.artistName || 'andwarhool234'}</h2>
                 <button>Favorite</button>
                 <button>Hide artist</button>
               </div>
               <div className={styles.artist_bio}>
-                <p className={`neutral5 ${styles.lives}`}><span className='neutral3'>10+</span> Lives shows</p>
+                <p className={`neutral5 ${styles.lives}`}><span className='neutral3'>{artist?.showPerformed || '10'}+</span> Lives shows</p>
                 <p className='neutral5'><span className='neutral3'>15</span> Years performing</p>
               </div>
-              <h2 className={`heading_6_medium ${styles.artist_name2}`}>Andy Warhool <img src="./icons/viral.png" alt="" /></h2>
-              <p className={styles.location}><MdOutlineLocationOn /> New York City</p>
-              <p className={styles.location}><GiMusicSpell /> Pop, Rock, Blues</p>
-              <p className={styles.bio}>It is a long established fact that a reader will be distracted by the eadable content of a page when looking at its layout.</p>
+              <h2 className={`heading_6_medium ${styles.artist_name2}`}>{artist?.artistName || 'Andy Warhool'} <img src="./icons/viral.png" alt="" /></h2>
+              <p className={styles.location}><MdOutlineLocationOn /> {artist?.location || 'New York'}</p>
+              <p className={styles.location}><GiMusicSpell /> {artist?.genres && ((artist?.genres).join(', ') ) || 'Rock, Pop, Jazz'}</p>
+              <p className={styles.bio}>{artist?.bio || 'It is a long established fact that a reader will be distracted by the eadable content of a page when looking at its layout.'}</p>
 
               <div className={styles.artist_shows}>
                 <p className={styles.next_shows}>Next Shows</p>
