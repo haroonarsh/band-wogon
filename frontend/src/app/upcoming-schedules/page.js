2494f6b335aefa111d91ba11574d3c33415516de
Haroon Arshad <arshadharoon216@gmail.com>
2025-02-22 23:37:24 +0500

    added loading animation & some styles

--- frontend/src/app/upcoming-schedules/page.js
+++ frontend/src/app/upcoming-schedules/page.js
@@ -91,7 +91,16 @@ function Page() {
     }, []);
 
     if (loading) {
-        return <div className={styles.loading}>Loading shows...</div>;
+        return (
+            /* From Uiverse.io by devAaus */ 
+            <div className="flex items-center justify-center h-screen bg-zinc-900">
+<div className="flex-col gap-4 w-full flex items-center justify-center">
+      <div className="w-20 h-20 border-4 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-blue-400 rounded-full">
+        <div className="w-16 h-16 border-4 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-t-red-400 rounded-full" />
+      </div>
+    </div>
+    </div>
+        )
     }
     const formatDate = (dateString) => {
         const options = { day: 'numeric', month: 'short', year: 'numeric' };
