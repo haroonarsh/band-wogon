0d2d7087b6d4773a6ab2b02f75b7f02b5d56b024
Haroon Arshad <arshadharoon216@gmail.com>
2025-02-22 23:36:41 +0500

    added loading animation

--- frontend/src/app/artist_profile/[id]/page.js
+++ frontend/src/app/artist_profile/[id]/page.js
@@ -50,7 +50,14 @@ function Artist_profile() {
 
   if (loading) {
     return (
-      <div>Loading...</div>
+      /* From Uiverse.io by devAaus */ 
+      <div className="flex items-center justify-center h-screen bg-zinc-900">
+      <div className="flex-col gap-4 w-full flex items-center justify-center">
+            <div className="w-20 h-20 border-4 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-blue-400 rounded-full">
+              <div className="w-16 h-16 border-4 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-t-red-400 rounded-full" />
+            </div>
+          </div>
+          </div>
     );
   }
 
